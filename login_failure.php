5db41607e77933689e245052186b163b037a47f1
Chandan Dwivedi <chandandwivedi89@gmail.com>
2020-02-27 18:55:15 +0530

    Create login_failure.php

---
+++ login_failure.php
@@ -0,0 +1,47 @@
+<html>
+	<head>
+		<style rel="stylesheet">
+			body{
+				display:flex;
+				justfy-content:center;
+				align-items:center;
+				margin:0;
+				padding:0;
+				height:100vh;
+			}
+			
+			.main{
+				padding:64px;
+				width:60%;
+				border:2px dashed green;
+				margin:auto;
+			}
+			
+			.container{
+				
+			}
+			
+		
+		</style>
+	</head>
+		
+	<body> 
+			<div class="main">
+			<form action="<?=baseURL("/",$qString)?>">
+			<div>
+				<label>
+					<input type="text" name="username">
+					</label>
+					
+				<label>
+					<input type="text" name="password">
+				</label>
+				
+				<button>Login</button>
+			</div>
+			</form>
+						
+		</div>
+		
+	</body>
+</html>
