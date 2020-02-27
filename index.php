c023fbe4b1a0275d0ffac65d2e0341aa2d36448f
Chandan Dwivedi <chandandwivedi89@gmail.com>
2020-02-27 18:54:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 	
-	//localhost/kodecamp/someurl
-	//localhost/kodecamp/index.php?route=someurl
 	require("./Controllers/TestController.php");
 	require("./Controllers/CollegeController.php");
 	require("./Controllers/CourseController.php");
@@ -11,9 +9,10 @@
 	require("./routes.php");
 	require("./lib/utility.php");
 
-	
+	error_reporting(E_ERROR | E_WARNING | E_PARSE);
 	class FrontController{
-		
+		private const LOGIN_KEY ="LOGIN_KEY";
+		 
 		public function processRoutes(){
 			global $routes;
 			$url = $_GET["route"];
@@ -23,14 +22,50 @@
 				die();
 			}
 			
+			$loggedInUser = $this->loggedInUser();
+			
+			if(!$loggedInUser){
+				$validUser=$this->authenticateRequest();
+				
+				if(!$validUser){
+					$queryString =$_SERVER["QUERY_STRING"];
+					$qString = str_replace("route=","",$queryString);
+					require("./Views/login_failure.php");
+					die();
+				}
+				$this->updateLoggedInUser($validUser);
+			}
+			$this->exeuteRoute($resourceRoute);
 		}
-		private function mappedRoute(){}
-		private function loggedInUser(){}
-		private function authenticateRequest(){}
-		private function updateLoggedInUser($loginValue){}
-		private function exeuteRoute($resourceRoute){}
-	}
+		 
+		private function mappedRoute($routes,$url){
+			$url=($url==="")? "base" : $url;
+			$mappedString =$routes[$url];
+			return ($mappedString ? $mappedString : null);
+		}
 		
+		private function loggedInUser(){
+			session_start();
+			return isset($_SESSION[self::LOGIN_KEY])? true : false;
+		}
+		
+		private function authenticateRequest(){
+			$loginController = new LoginController();
+			$validUser = $loginController->authenticate();
+			return $validUser?true:false;
+		}
+		
+		private function updateLoggedInUser($loginValue){
+			$_SESSION[self::LOGIN_KEY]= $loginValue;
+		}
+		private function exeuteRoute($resourceRoute){
+			$parts = explode("/",$resourceRoute);
+			$className =$parts[0];
+			$methodName =$parts[1];
+			$ctrlObj =new $className();
+			$ctrlObj->{$methodName}();
+		}
+	}
 	$frontController = new FrontController();
 	$frontController -> processRoutes();
 	
